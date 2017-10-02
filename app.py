c7646db32dffb462825cb8f505e4a2bc11c5995d
FVI <test@examle.com>
2017-10-02 18:06:29 -0400

    last ver

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, flash, jsonify, render_template, request
+import requests
+
+import urllib.request
+import json
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+dinfo={}
+
+@app.route('/', methods=['POST', 'GET'])
+
+def my_search():
+    if request.method == 'POST':
+        task = request.form['task']
+        url="https://www.googleapis.com/books/v1/volumes?q="
+        max_result="maxResults=20"
+        final_url=url+task+'&'+max_result
+        json_obj=urllib.request.urlopen(final_url)
+        data=json.load(json_obj)
+        for  v in data['items']:
+                mylink = v['volumeInfo']['previewLink']
+                if isinstance(v, dict):
+                    for x in v['volumeInfo']['imageLinks'].values():
+                            for y in v['volumeInfo']['authors']:
+                                
+                                dinfo[v['id']]=x,y,mylink 
+    
+        return render_template('index.html',title="Get It Done!", dinfo=dinfo) 
+        
+    
+app.run()
